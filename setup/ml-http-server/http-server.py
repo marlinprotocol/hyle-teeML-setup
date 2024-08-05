0ea07012fd8a55a820f1d3484a7fe799690ab9d3
AyushKaul <ayushk.kaul3@gmail.com>
2024-08-06 00:56:51 +0530

    small - remove un-necessary comments

--- setup/ml-http-server/http-server.py
+++ setup/ml-http-server/http-server.py
@@ -23,26 +23,6 @@ def predict():
 
 # Comment out or remove below lines to run in prod
 # if __name__ == '__main__':
-#     training_data = FerPlus('Training', '/home/marlin/FERPlus/fer2013new.csv', '/home/marlin/FERPlus/')
-#     batch_size = 2**12
-
-#     # Create data loaders.
-#     train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=True)
-#     img_list = []
-
-#     for X, y in training_data:
-#         img_list.append(X.numpy().flatten())
-
-#     print(len(img_list))
-
-#     img_dataframe = pd.DataFrame(img_list)
-
-#     # Load the local classifier
-#     print("Loading XGBClassifier...")
-#     model = joblib.load("serialized_optimized.pkl")
-    
-#     result = prediction(img_list[0], model)
-#     print("Prediction result: ", result)
 #     app.run(debug=True)
 
 # To run the app in production, use the following command:
